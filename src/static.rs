07ce379e1b8075361f0e8500c6917db87baee402
p0lyw0lf <p0lyw0lf@protonmail.com>
2024-02-04 19:49:01 -0500

    new static dir location

--- src/static.rs
+++ src/static.rs
@@ -5,6 +5,6 @@ use crate::AppState;
 
 pub fn register(app: Router<AppState>) -> Router<AppState> {
     // See the esbuild project in the `static` folder
-    app.nest_service("/static", ServeDir::new("static/dist"))
+    app.nest_service("/static/dist", ServeDir::new("static/dist"))
         .nest_service("/assets", ServeDir::new("assets"))
 }
