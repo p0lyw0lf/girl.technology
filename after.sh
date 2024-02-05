87b621bddea9184c79e5701c87923fcc8d42ee5e
p0lyw0lf <p0lyw0lf@protonmail.com>
2024-02-04 23:26:46 -0500

    delay before fetching binary

--- after.sh
+++ after.sh
@@ -1,7 +1,11 @@
 #!/bin/env sh
 
 sudo -n systemctl stop bot-girl-technology.service
-# Thanks, cargo-dist!
+# It seems that github takes a second to update the latest release URL, even
+# after the release webhook is fired. So, we need to wait a few seconds before
+# attempting to fetch
+sleep 5
+# cargo-dist makes this really easy, thanks axo.dev!
 curl --proto '=https' --tlsv1.2 -LsSf https://github.com/p0lyw0lf/girl.technology/releases/latest/download/girl_technology-installer.sh | sh
 sudo -n systemctl daemon-reload
 sudo -n systemctl start bot-girl-technology.service
