9b9eef04865aad1cca33dc6141133e09f8f6d9c2
PolyWolf <31190026+p0lyw0lf@users.noreply.github.com>
2024-08-17 02:53:28 +0000

    update after.sh

--- after.sh
+++ after.sh
@@ -1,11 +1,11 @@
 #!/bin/env sh
 
-sudo -n systemctl stop bot-girl-technology.service
+systemctl --user stop girl.technology.service
 # It seems that github takes a second to update the latest release URL, even
 # after the release webhook is fired. So, we need to wait a few seconds before
 # attempting to fetch
 sleep 5
 # cargo-dist makes this really easy, thanks axo.dev!
 curl --proto '=https' --tlsv1.2 -LsSf https://github.com/p0lyw0lf/girl.technology/releases/latest/download/girl_technology-installer.sh | sh
-sudo -n systemctl daemon-reload
-sudo -n systemctl start bot-girl-technology.service
+systemctl --user daemon-reload
+systemctl --user start girl.technology.service
